474f33e798fcfb3508bd368fd7c31ff7721036e0
𝓒𝓪𝓲𝓸 𝓟𝓾𝔂𝓸𝓵 𝓛𝓮𝓰𝓾𝓲𝔃𝓪 <caiopuyolleguiza@gmail.com>
2025-07-27 20:08:02 -0300

    Update header.js

--- components/header.js
+++ components/header.js
@@ -4,16 +4,16 @@ function agregarHeader(container) {
     <div class="header-container custom-wide-container">
       <div class="columns is-mobile is-vcentered is-variable is-1 header">
         <div class="column is-narrow">
-          <a href="index.html">
+          <a href="./index.html">
           <img src="./assets/logos/main-logo-caio.png" alt="Logo" class="header-logo">
           </a>
         </div>
 
         <div class="column is-hidden-touch has-text-right">
           <nav class="desktop-nav">
-            <a href="/portfolio.html">Portfolio</a>
-            <a href="/servicios.html">Servicios</a>
-            <a href="/contacto.html">Contacto</a>
+            <a href="./portfolio.html">Portfolio</a>
+            <a href="./servicios.html">Servicios</a>
+            <a href="./contacto.html">Contacto</a>
           </nav>
         </div>
 
@@ -31,9 +31,9 @@ function agregarHeader(container) {
       <div class="menu-content box">
         <button id="close-header-menu" class="delete is-large" aria-label="close"></button>
         <ul>
-          <li><a href="/portfolio.html">Portfolio</a></li>
-          <li><a href="/servicios.html">Servicios</a></li>
-          <li><a href="/contacto.html">Contacto</a></li>
+          <li><a href="./portfolio.html">Portfolio</a></li>
+          <li><a href="./servicios.html">Servicios</a></li>
+          <li><a href="./contacto.html">Contacto</a></li>
         </ul>
       </div>
     </div>
