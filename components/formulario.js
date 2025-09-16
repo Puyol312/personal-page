44b9c8aac0b4dbf22bed2e5a1f59dfbc83661dc0
𝓒𝓪𝓲𝓸 𝓟𝓾𝔂𝓸𝓵 𝓛𝓮𝓰𝓾𝓲𝔃𝓪 <caiopuyolleguiza@gmail.com>
2025-09-16 14:39:02 -0300

    Update formulario.js

--- components/formulario.js
+++ components/formulario.js
@@ -62,7 +62,7 @@ function activarFormulario() {
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify({
         to: email,
-        message: `From:${jsonFormData.email} \n Mensaje: ${jsonFormData.mensaje}`
+        message: `Name:${jsonFormData.nombre} \n\n From: ${jsonFormData.email} \n\n Mensaje: ${jsonFormData.mensaje}`
       })
     })
     .then(response => response.json())
